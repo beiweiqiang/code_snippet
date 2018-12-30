d8cd5a5ca3f327302536fef00bb3d4f7838079a7
beiweiqiang <tribal.bei@onedollartech.com>
2018-12-30 10:08:42 +0800

    Update start.js

--- nodejs/enable_import/start.js
+++ nodejs/enable_import/start.js
@@ -1,6 +1,8 @@
 /**
  * 先要安装依赖:
  * npm install babel-register babel-preset-env --save-dev
+ *
+ * 然后使用 node start.js 启动, 而不是 node server.js
  */
 
 // Transpile all code following this line with babel and use 'env' (aka ES6) preset.
@@ -9,5 +11,5 @@ require('babel-register')({
 })
 
 // Import the rest of our application.
-module.exports = require('./get');
+module.exports = require('./server');
 

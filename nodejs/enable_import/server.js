cacd708e7431e80a1769b77edd25aef858fb6a6f
beiweiqiang <tribal.bei@onedollartech.com>
2018-12-30 10:08:05 +0800

    Create server.js

---
+++ nodejs/enable_import/server.js
@@ -0,0 +1,13 @@
+/*
+ * 这是 import 的例子
+ */
+
+import express from 'express'
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
